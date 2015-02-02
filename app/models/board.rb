3dff264126582cc677ad73bea6f3aeda86923db3
Keleigh Lynch <keleigh@Keleigh-Lynchs-MacBook-Pro.local>
2015-02-02 18:35:01 -0500

    Initial commit

---
+++ app/models/board.rb
@@ -0,0 +1,8 @@
+class Board < ActiveRecord::Base
+	validates :name, :presence => true
+	validates :user, :presence => true
+	validates :about, :presence => true 
+
+	belongs_to :user
+	has_many :pins 
+end
